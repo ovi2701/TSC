2e9165665232114791537a6c820f21a2341c5984
ovi2701 <botsch_ovidiu27@yahoo.com>
2022-03-28 11:49:09 +0300

    laborator5

--- lab2/lab01_testbench-interface/instr_register_test.sv
+++ lab2/lab01_testbench-interface/instr_register_test.sv
@@ -7,13 +7,18 @@
 
 module instr_register_test
   import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
-  (tb_ifc.TB intf_lab2);
+  (
+  tb_ifc.TB intf_lab2
+  );
 
   //timeunit 1ns/1ns;
 
+  //seed-ul reprezinta valoarea initiala cu care se incepe random-izarea si se foloseste pt a stabiliza codul, pt a avea aceleasi rez pe cod cu aceleasi valori random
   int seed = 555;
 
+  //tipmul este 0
   initial begin
+	//valorile din display afisate in transcript la inceput
     $display("\n\n***********************************************************");
     $display(    "***  THIS IS NOT A SELF-CHECKING TESTBENCH (YET).  YOU  ***");
     $display(    "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
@@ -21,32 +26,37 @@ module instr_register_test
     $display(    "***********************************************************");
 
     $display("\nReseting the instruction register...");
-    intf_lab2.cb.write_pointer  = 5'h00;         // initialize write pointer
-    intf_lab2.cb.read_pointer   = 5'h1F;         // initialize read pointer
-    intf_lab2.cb.load_en        = 1'b0;          // initialize load control line
+	$display("first header");
+    intf_lab2.cb.write_pointer  <= 5'h00;         // initialize write pointer
+    intf_lab2.cb.read_pointer   <= 5'h1F;         // initialize read pointer
+    intf_lab2.cb.load_en        <= 1'b0;          // initialize load control line
     intf_lab2.cb.reset_n       <= 1'b0;          // assert reset_n (active low)
-    repeat (2) @(posedge intf_lab2.cb.clk) ;     // hold in reset for 2 clock cycles
-    intf_lab2.cb.reset_n        = 1'b1;          // deassert reset_n (active low)
+    repeat (2) @(posedge intf_lab2.cb) ;     // hold in reset for 2 clock cycles //asteptam doua fronturi pozitive de ceas
+    intf_lab2.cb.reset_n        <= 1'b1;          // deassert reset_n (active low)
+	
+	//o functie are timpi de simulare 0, iar taskul poate avea valori temporale, adica poate avea timpi de simulare diferiti de 0!!!!
 
     $display("\nWriting values to register stack...");
-    @(posedge intf_lab2.cb.clk) intf_lab2.cb.load_en = 1'b1;  // enable writing to register
+    @(posedge intf_lab2.cb) intf_lab2.cb.load_en <= 1'b1;  // enable writing to register
     repeat (3) begin
-      @(posedge intf_lab2.cb.clk) randomize_transaction;
-      @(negedge intf_lab2.cb.clk) print_transaction;
+      @(posedge intf_lab2.cb) randomize_transaction;
+      @(negedge intf_lab2.cb) print_transaction;
     end
-    @(posedge intf_lab2.cb.clk) intf_lab2.cb.load_en = 1'b0;  // turn-off writing to register
+    @(posedge intf_lab2.cb) intf_lab2.cb.load_en <= 1'b0;  // turn-off writing to register
 
     // read back and display same three register locations
     $display("\nReading back the same register locations written...");
-    for (int i=0; i<=2; i++) begin
+	repeat(3) begin
+    for (int i=2; i>=0; i--) begin
       // later labs will replace this loop with iterating through a
       // scoreboard to determine which addresses were written and
       // the expected values to be read back
-      @(posedge intf_lab2.cb.clk) intf_lab2.read_pointer = i;
-      @(negedge intf_lab2.cb.clk) print_results;
+      @(posedge intf_lab2.cb) intf_lab2.cb.read_pointer <= i;
+      @(negedge intf_lab2.cb) print_results;
     end
-
-    @(posedge intf_lab2.cb.clk) ;
+	end
+	
+    @(posedge intf_lab2.cb) ;
     $display("\n***********************************************************");
     $display(  "***  THIS IS NOT A SELF-CHECKING TESTBENCH (YET).  YOU  ***");
     $display(  "***  NEED TO VISUALLY VERIFY THAT THE OUTPUT VALUES     ***");
@@ -66,7 +76,7 @@ module instr_register_test
     static int temp = 0;
     intf_lab2.cb.operand_a     <= $random(seed)%16;                 // between -15 and 15
     intf_lab2.cb.operand_b     <= $unsigned($random)%16;            // between 0 and 15
-    intf_lab2.cb.opcode        <= opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type
+    intf_lab2.cb.opcode        <= opcode_t'($unsigned($random)%8);  // between 0 and 7, cast to opcode_t type  //converteste index in string
     intf_lab2.cb.write_pointer <= temp++;
   endfunction: randomize_transaction
 
@@ -75,13 +85,15 @@ module instr_register_test
     $display("  opcode = %0d (%s)", intf_lab2.cb.opcode, intf_lab2.cb.opcode.name);
     $display("  operand_a = %0d",   intf_lab2.cb.operand_a);
     $display("  operand_b = %0d\n", intf_lab2.cb.operand_b);
+	$display("Printing transaction: %d ns ",$time);
   endfunction: print_transaction
 
   function void print_results;
     $display("Read from register location %0d: ", intf_lab2.cb.read_pointer);
-    $display("  opcode = %0d (%s)", intf_lab2.cb.instruction_word.opc, intf_lab2.instruction_word.opc.name);
+    $display("  opcode = %0d (%s)", intf_lab2.cb.instruction_word.opc, intf_lab2.cb.instruction_word.opc.name);
     $display("  operand_a = %0d",   intf_lab2.cb.instruction_word.op_a);
     $display("  operand_b = %0d\n", intf_lab2.cb.instruction_word.op_b);
+	$display("Printing results: %d ns ",$time);
   endfunction: print_results
 
 endmodule: instr_register_test
--- lab2/lab01_testbench-interface/tb_ifc.sv
+++ lab2/lab01_testbench-interface/tb_ifc.sv
@@ -16,7 +16,7 @@ interface tb_ifc (input logic clk);
   address_t      write_pointer, read_pointer;
   instruction_t  instruction_word;
   
-  clocking cb @(posedge clk);
+  clocking cb @(clk);
 	output   load_en;
 	output   reset_n;
 	output   operand_a;
